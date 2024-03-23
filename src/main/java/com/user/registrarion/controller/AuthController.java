e5d73e0488cb2b81d704911a9178f11369bfd8da
debora-evilaine <sousxdebora@gmail.com>
2024-03-23 13:56:22 -0300

    feat: Adding route and html to homepage

--- src/main/java/com/user/registrarion/controller/AuthController.java
+++ src/main/java/com/user/registrarion/controller/AuthController.java
@@ -1,15 +1,18 @@
 package com.user.registrarion.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AuthController {
 
     //This method handles a 'get' request to the homepage
     @GetMapping("/home")
-    public String home(){
-        return "home";
+    public ModelAndView home(){
+        ModelAndView mv = new ModelAndView("home.html");
+        return mv;
     }
 
 }
